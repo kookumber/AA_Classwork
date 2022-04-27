da04629c6dc2606f2aa220c769d40669f9027589
Quang Tran <quang.tran.769869@gmail.com>
2022-04-27 14:51:44 -0700

    fix battleship

--- W2D3/battleship_project/lib/board.rb
+++ W2D3/battleship_project/lib/board.rb
@@ -5,10 +5,7 @@ class Board
 
     def initialize(num)
         @num = num
-        @grid = []
-        (1..num).each do |i|
-            @grid << Array.new(num, :N)
-        end
+        @grid = Array.new(num) {Array.new(num, :N)}
         @size = num * num
     end
 
@@ -35,44 +32,51 @@ class Board
     #----- START PART 2 ----->
 
     def attack(position) #position is still arr like [2, 4]
-        if self.[](position) == :S
-            self.[]=(position, :H)
+        if self[position] == :S
+            self[position] = :H
             puts "you sunk my battleship!"
             return true
         else
-            self.[]=(position, :X)
+            self[position] = :X
             return false
         end
     end
 
     def place_random_ships
-        quarter = size / 4 #get num of times to replace :N with :S
-        arr = [] #empty array we set to fill in with grid positions
+        quarter = @size / 4 #get num of times to replace :N with :S
+        # arr = [] #empty array we set to fill in with grid positions
         range = @grid.length - 1 #length of grid while will give us max position range
 
-        quarter.times {|i| arr << [rand(0..range), rand(0..range)]} #add to arr random positions
+        # quarter.times {|i| arr << [rand(0..range), rand(0..range)]} #add to arr random positions
 
-        while arr.uniq.length < quarter
-            arr << [rand(0..range), rand(0..range)]
-        end #makes sure we're not using same positions multiple times
+        # while arr.uniq.length < quarter
+        #     arr << [rand(0..range), rand(0..range)]
+        # end #makes sure we're not using same positions multiple times
 
-        final_arr = arr.uniq #gives final grid that should have uniq positions count equiv to quarter
+        # final_arr = arr.uniq #gives final grid that should have uniq positions count equiv to quarter
 
-        final_arr.each do |pos| #loops through the final arr and reassigns grid positions with :S
-            row = pos[0]
-            col = pos[1]
-            @grid[row][col] = :S
+        # final_arr.each do |pos| #loops through the final arr and reassigns grid positions with :S
+        #     row = pos[0]
+        #     col = pos[1]
+        #     @grid[row][col] = :S
+
+        while num_ships < quarter
+            rand_r = rand(0..range)
+            rand_c = rand(0..range)
+            @grid[rand_r][rand_c] = :S
         end
     end
 
     def hidden_ships_grid
-        hidden_grid = []
-        @grid.each {|row| hidden_grid << row.uniq}
-
-        hidden_grid.each_with_index do |row, row_idx|
-            row.each_with_index {|col, col_idx| hidden_grid[row_idx][col_idx] = :N if hidden_grid[row_idx][col_idx] == :S }
+        @grid.map do |row|
+            row.map do |ele|
+                if ele == :S
+                    :N
+                else
+                    ele
+                end
+            end
         end
-        hidden_grid
     end
 
     def self.print_grid(grid)
