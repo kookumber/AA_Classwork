402a3b8fe2282e03899d39eeea679760b8f92d25
Quang Tran <quang.tran.769869@gmail.com>
2022-05-08 22:21:07 -0700

    committing W3D5 polytreenode classwork

---
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -0,0 +1,59 @@
+class PolyTreeNode
+
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent = node
+        return self if node == nil
+        if node.parent != nil
+            node.parent.children.delete(self)
+        end
+        node.children << self if !node.children.include?(self)
+    end
+
+    def add_child(child_node)
+        if !self.children.include?(child_node)
+            child_node.parent=(self)
+        end
+    end
+
+    def remove_child(child_node)
+        if self.children.include?(child_node)
+            child_node.parent = nil
+        else
+            raise 'not a child'
+        end
+    end
+
+    def dfs(target_val)
+        return self if value == target_val
+        return nil if self == nil
+       
+        self.children.each do |child|
+            search_result = child.dfs(target_val)
+            return search_result unless search_result.nil?
+        end
+
+        nil
+    end
+
+    def bfs(target_val)
+        return self if value == target_val #good
+        queue = [self] #good
+        until queue.empty? #good
+            ele = queue.shift
+             ele.children.each do |child|
+                # return child if child == target_val
+                queue << child
+                # bfs(target_val)
+            end
+        end
+    end
+    
+end 
